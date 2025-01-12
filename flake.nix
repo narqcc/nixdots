f6db9ac1b328887b4dbef52e0557a835e03110ef
narqcc <186345316+narqcc@users.noreply.github.com>
2025-01-12 16:24:19 -0500

    flakelight template

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakelight.url = "github:nix-community/flakelight";
+  };
+  outputs = { flakelight, ... }@inputs:
+    flakelight ./. {
+      inherit inputs;
+    };
+}
