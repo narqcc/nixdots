8e67aaa3b5c667bf59f133428b6e436ca6f81def
narqcc <186345316+narqcc@users.noreply.github.com>
2025-01-13 16:28:38 -0500

    add iso configuration

---
+++ nix/hosts/iso.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ... }:
+{
+  modules = [
+    "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
+    "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+    inputs.self.nixosModules.common
+  ];
+
+  nixpkgs = {
+     hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = lib.mkForce ["zfs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
+  }
+
+  services = {
+    qemuGuest.enable = true;
+    openssh.settings.PermitRootLogin = lib.mkForce "yes";
+  };
+
+  users.extraUsers.root.password = "nixos";
+}
