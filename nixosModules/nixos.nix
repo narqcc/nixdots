afbeab40e5af3932579f19fe4d7be9dc0e950b62
narqcc <186345316+narqcc@users.noreply.github.com>
2025-01-23 09:58:27 -0500

    nix settings

--- nixosModules/nixos.nix
+++ nixosModules/nixos.nix
@@ -1,6 +1,10 @@
 {
+  system.stateVersion = "24.11";
   nix = {
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      auto-optimise-store = true;
+    };
     extraOptions = "experimental-features = nix-command flakes";
   };
 }
