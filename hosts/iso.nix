b4b8409437befeee07a0301286054fd004191475
narqcc <186345316+narqcc@users.noreply.github.com>
2025-01-23 10:11:05 -0500

    iso defaults

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -3,4 +3,18 @@
   imports = [
     ../nixosModules/nixos.nix
   ];
+
+  services = {
+    qemuGuest.enable = true;
+    openssh.settings.PermitRootLogin = lib.mkForce "yes";
+  };
+
+  networking.hostName = "nixos";
+  home = {
+    username = lib.mkDefault "nixos";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+  };
+
+  users.extraUsers.root.password = "nixos";
+
 }
