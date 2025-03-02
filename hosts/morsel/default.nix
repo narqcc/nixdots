6aa56b33b6a781b0c0ea6e0532bdc4a4c39278fa
narqcc <186345316+narqcc@users.noreply.github.com>
2025-03-02 10:01:44 -0500

    add services to morsel

--- hosts/morsel/default.nix
+++ hosts/morsel/default.nix
@@ -1,11 +1,12 @@
-{}:
 {
   imports = [
     ../../nixosModules/nixos.nix
     ../../nixosModules/nixpkgs.nix
+    ../../nixosModules/services/efi-boot.nix
+    ../../nixosModules/services/openssh.nix
+    ../../nixosModules/services/plasma6.nix
     ./fs.nix
     ./hw.nix
     ./net.nix
   ];
-
 }
